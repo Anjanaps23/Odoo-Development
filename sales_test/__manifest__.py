aa7dfe55e7f2f773e8a553cc3f1597e4b9a4bd20
Anjana <anjanaps@akiraplc.com>
2024-05-03 12:14:38 +0530

    Add functionality to automatically create delivery and send notification email upon order confirmation
    This commit enhances the `action_confirm` method in the `SaleOrderInherit` class to automate the creation of delivery orders and send notification emails to customers upon order confirmation.
    
    - Upon order confirmation, a new delivery order is created for each order line item.
    - The delivery order includes relevant details such as product information, quantity, and expected delivery date.
    - A notification email containing the delivery status and expected delivery date is sent to the customer.

---
+++ sales_test/__manifest__.py
@@ -0,0 +1,18 @@
+
+{
+    'name': 'Test',
+    'version': '1.6',
+    'category': 'Education',
+    "sequence": 3,
+    'summary': 'School Management',
+    'complexity': "User Friendly",
+    'author': 'Akira Software Solutions Pvt. Ltd.',
+    'website': 'http://www.akiraplc.com',
+    'depends': ['sale'],
+    'data': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ sales_test/models/sale_order_inherit.py
@@ -0,0 +1,48 @@
+from odoo import models, fields, api
+
+
+class SaleOrderInherit(models.Model):
+    _inherit = 'sale.order'
+
+    def action_confirm(self):
+        res = super(SaleOrderInherit, self).action_confirm()
+        for order in self:
+            order.write({'picking_ids': [(5, 0, 0)]})
+            for line in order.order_line:
+                delivery = self.env['stock.picking'].create({
+                    'partner_id': order.partner_shipping_id.id,
+                    'picking_type_id': order.warehouse_id.out_type_id.id,
+                    'sale_id': order.id,
+                    'move_ids_without_package': [(0, 0, {
+                        'name': line.product_id.name,
+                        'product_id': line.product_id.id,
+                        'product_uom_qty': line.product_uom_qty,
+                        'product_uom': line.product_uom.id,
+                        'location_id': line.product_id.property_stock_production.id,
+                        'location_dest_id': order.partner_shipping_id.property_stock_customer.id,
+                        'origin': order.name,
+                        'sale_line_id': line.id,
+                    })]
+                })
+                order.send_delivery_email_notification(delivery.scheduled_date)
+        return res
+
+    def send_delivery_email_notification(self, date):
+        for rec in self:
+            mail = self.env['mail.mail'].sudo().create({
+                'subject': 'Delivery status',
+                'email_to': rec.partner_id.email,
+                'email_from': rec.company_id.email,
+                'body_html': """<div>
+                                <p>Hi """ + ' ' + str(rec.partner_id.name) + """,</p>
+                                 <p>The status of the delivery of your order is as follows:</p>
+                                <p>Delivery status: """ + str(rec.delivery_status) + """</p>
+                                <p>Expected delivery: """ + str(date) + """</p>
+
+                                <br/>
+                           </div>"""
+
+            })
+            mail.send()
+
+
